51a3042783a45f971179d87826aa5e623daaa791
lethanhdatctu <54407533+thanhdat24@users.noreply.github.com>
2022-01-12 23:07:13 +0700

    Feat: Sending JWT via Cookie

--- app.js
+++ app.js
@@ -4,14 +4,21 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRoutes = require('./routers/tourRouters');
 const userRoutes = require('./routers/userRouters');
-
+const rateLimit = require('express-rate-limit');
 const app = express();
 
-// 1) Middleware
+// 1) Global Middleware
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windows: 60 * 60 * 1000,
+  message: 'Too many requests from this IP. Please try again in an hour!',
+});
+app.use('/api', limiter);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
