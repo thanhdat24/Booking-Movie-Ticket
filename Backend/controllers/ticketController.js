2306d5e2f6371bdfd032b60bddff893db6e7a27a
Thành Đạt <54407533+thanhdat24@users.noreply.github.com>
2022-06-12 00:22:54 +0700

    Feat: Send email

--- Backend/controllers/ticketController.js
+++ Backend/controllers/ticketController.js
@@ -2,11 +2,31 @@ const Ticket = require('../models/ticketModel');
 const factory = require('./handlerFactory');
 const catchAsync = require('../utils/catchAsync');
 const ShowTime = require('../models/showtimeModel');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+let transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: process.env.EMAIL_APP, // generated ethereal user
+    pass: process.env.EMAIL_APP_PASSWORD, // generated ethereal password
+  },
+});
+
+let newTicket = null;
+let calculateTimeout = (dateShow) => {
+  const fakeThoiLuong = 120;
+  const timeInObj = new Date(dateShow);
+  const timeOutObj = new Date(timeInObj.getTime() + fakeThoiLuong * 60 * 1000);
+  return timeOutObj.toLocaleTimeString([], { hour12: false }).slice(0, 5);
+};
 
 exports.createTicket = catchAsync(async (req, res, next) => {
   const { idShowtime, seatCodes } = req.body;
   const user = req.user;
-  ShowTime.findById(idShowtime)
+  await ShowTime.findById(idShowtime)
     .populate('idMovie')
     .populate('idTheater')
     .then((showtime) => {
@@ -29,7 +49,7 @@ exports.createTicket = catchAsync(async (req, res, next) => {
           message: 'Ghế đã được đặt!',
           notAvaiSeat: invalidSeat,
         });
-      const newTicket = new Ticket({
+      newTicket = new Ticket({
         idShowtime,
         userId: user,
         seatList: seatCodes.map((seat) => ({
@@ -50,10 +70,141 @@ exports.createTicket = catchAsync(async (req, res, next) => {
         status: 'success',
         data: newTicket,
       });
+      req.ticket = newTicket;
+      req.showtime = showtime;
+
+      // console.log('req.ticket', req.ticket);
+      // console.log('req.showtime', req.showtime);
     })
     .catch((err) => {
       res.status(400).json({ message: err });
     });
+  const amountTicket = req.body.seatCodes.length;
+  const { id, totalPrice, createdAt, price } = req.ticket;
+  const { dateShow, idTheaterCluster, idTheater, idMovie } = req.showtime;
+  let formatDateTimeShow = new Date(dateShow)
+    .toLocaleTimeString([], { hour12: false })
+    .slice(0, 5);
+
+  let formatDateShow = new Date(dateShow)
+    .toLocaleDateString('en-GB')
+    .slice(0, 10);
+  let formatDateCreateAt = new Date(createdAt)
+    .toLocaleDateString('en-GB')
+    .slice(0, 10);
+
+  let formatDateCreateAtTime = new Date(createdAt).toLocaleTimeString();
+
+  await transporter.sendMail({
+    from: `"Giao Dich Thanh Cong " <ltd.ctu@gmail.com>`, // sender address
+    to: 'thanhledatomon@gmail.com', // list of receivers
+    subject: 'EMAIL XÁC NHẬN ĐẶT VÉ THÀNH CÔNG', // Subject line
+    // text: "Hello world?", // plain text body
+    html: `   <div style="background-color: #ff7f00 ;text-align: center;padding: 10px; border-radius: 10px; width: 80%">
+        <div style=" color: white; margin-bottom: 10px;">
+              <h3>Chúc mừng bạn!</h3>
+              <span>Việc mua vé online của bạn đã thành công. Star Movie xin chân thành cám ơn bạn đã chọn chúng tôi để
+                  phục vụ nhu cầu giải trí của bạn. Bạn vui lòng xem thông tin đặt vé dưới đây</span>
+          </div>
+
+              <table style="background-color: #F3E9DD;width: 80%; margin: 0 auto;padding: 20px;text-align: left;">
+                  <tbody>
+                      <tr>
+                          <td>Mã vé:</td>
+                          <th >${id}</th>
+                      </tr>
+                      <tr>
+                          <td>Tên phim:</td>
+                          <th >
+
+    ${idMovie.name}
+                          </th>
+                      </tr>
+                      <tr>
+                          <td>Rạp:</td>
+                          <th >${idTheaterCluster.name}</th>
+                      </tr>
+                       <tr>
+                          <td>Phòng chiếu:</td>
+                          <th >${idTheater.name}</th>
+                      </tr>
+                      <tr>
+                          <td>Suất chiếu: </td>
+                          <th > ${formatDateShow}, ${formatDateTimeShow} ~ ${calculateTimeout(
+      dateShow
+    )}</th>
+                      </tr>
+                      <tr>
+                          <td>Ghế:</td>
+                          <th ><span>${req.body.seatCodes.join(
+                            ', '
+                          )}</span></th>
+                      </tr>
+                      <tr>
+                          <td>Phương thức thanh toán:</td>
+                          <th ><b>ZaloPay</b></th>
+                      </tr>
+                      <tr>
+                          <td>Thời gian thanh toán:</td>
+                          <th >${formatDateCreateAt}, ${formatDateCreateAtTime}</th>
+                      </tr>
+                        <tr>
+                          <td>Giá:</td>
+                          <th >${amountTicket} x ${price.toLocaleString(
+      'vi-VI'
+    )} vnđ</th>
+                      </tr>
+
+                           <tr style="border-collapse:collapse;">
+                            <td style="padding:0;Margin:0;padding-bottom:10px">
+                               <table border="0" width="100%" height="100%" cellpadding="0" cellspacing="0" style="border-collapse:collapse;border-spacing:0px"> 
+                     <tbody><tr style="border-collapse:collapse"> 
+                      <td style="padding:0;Margin:0px;border-bottom:1px dotted #cccccc;background:none;height:1px;width:100%;margin:0px"></td> 
+                     </tr> 
+                   </tbody></table>
+                            </td>
+                               <td style="padding:0;Margin:0;padding-bottom:10px">
+                               <table border="0" width="100%" height="100%" cellpadding="0" cellspacing="0" style="border-collapse:collapse;border-spacing:0px"> 
+                     <tbody><tr style="border-collapse:collapse"> 
+                      <td style="padding:0;Margin:0px;border-bottom:1px dotted #cccccc;background:none;height:1px;width:100%;margin:0px"></td> 
+                     </tr> 
+                   </tbody></table>
+                            </td>
+                           </tr>
+                        <tr>
+                          <td><b>KHUYẾN MÃI</b></td>
+                      </tr>
+                              <tr style="border-collapse:collapse;">
+                            <td style="padding:0;Margin:0;padding-bottom:10px">
+                               <table border="0" width="100%" height="100%" cellpadding="0" cellspacing="0" style="border-collapse:collapse;border-spacing:0px"> 
+                     <tbody><tr style="border-collapse:collapse"> 
+                      <td style="padding:0;Margin:0px;border-bottom:1px dotted #cccccc;background:none;height:1px;width:100%;margin:0px"></td> 
+                     </tr> 
+                   </tbody></table>
+                            </td>
+                               <td style="padding:0;Margin:0;padding-bottom:10px">
+                               <table border="0" width="100%" height="100%" cellpadding="0" cellspacing="0" style="border-collapse:collapse;border-spacing:0px"> 
+                     <tbody><tr style="border-collapse:collapse"> 
+                      <td style="padding:0;Margin:0px;border-bottom:1px dotted #cccccc;background:none;height:1px;width:100%;margin:0px"></td> 
+                     </tr> 
+                   </tbody></table>
+                            </td>
+                           </tr>
+
+                      <tr>
+                          <td>Tổng Cộng:</td>
+                          <th ><span style="font-size:24px">${totalPrice.toLocaleString(
+                            'vi-VI'
+                          )} vnđ</span></th>
+                      </tr>
+                  </tbody>
+
+              </table>
+
+  </div>
+      </div>
+                      `,
+  });
 });
 
 exports.getAllTicket = factory.getAll(Ticket, { path: 'showtimes' });
--- Backend/models/ticketModel.js
+++ Backend/models/ticketModel.js
@@ -1,24 +1,30 @@
 const mongoose = require('mongoose');
 const { seatSchema } = require('./seatModel');
-const ticketSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'User',
+const ticketSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+    },
+    showtimeId: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'ShowTimes',
+    },
+    seatList: [seatSchema],
+    price: { type: Number, required: true },
+    totalPrice: { type: Number, required: true },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      // loai bo thuoc tinh createdAt
+      select: false,
+    },
   },
-  showtimeId: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'ShowTimes',
-  },
-  seatList: [seatSchema],
-  price: { type: Number, required: true },
-  totalPrice: { type: Number, required: true },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    // loai bo thuoc tinh createdAt
-    select: false,
-  },
-});
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
 ticketSchema.pre(/^find/, function (next) {
   this.populate({
